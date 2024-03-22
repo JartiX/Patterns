3e6deab562019c70b6478feec88ac17edc358976
JartiX <artempilavin@gmail.com>
2024-03-22 21:58:27 +0800

    Update storage_model.py

--- Src/Models/storage_model.py
+++ Src/Models/storage_model.py
@@ -1,15 +1,40 @@
 from Src.reference import reference
 from Src.exceptions import exception_proxy
 
+#
+# Модель склада
+#
 class storage_model(reference):
-    __address: str = None
-
-    def __init__(self, address):
-        super().__init__(address)
-        self.__address = address
-
+    _address: str = ""
+    
     @property
-    def address(self):
-        return self.__address
+    def address(self) -> str:
+        """
+            Адрес
+
+        Returns:
+            _type_: _description_
+        """
+        return self._address
     
-    
+    @address.setter
+    def address(self, value:str):
+        """
+            Адрес
+        Args:
+            value (str): _description_
+        """
+        exception_proxy.validate(value, str)
+        self._address = value
+        
+    @staticmethod    
+    def create_default() -> reference:
+        """
+            Сформировать склад по умолчанию
+        Returns:
+            reference: _description_
+        """
+        storage = storage_model("default")
+        storage.address = "г. Москва. ул. Академика Королева, 10"
+        
+        return storage    
