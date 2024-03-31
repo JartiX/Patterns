9b317b15627c56deb8692ae8a2fddeea5f34d452
JartiX <artempilavin@gmail.com>
2024-03-31 18:09:41 +0800

    upd

--- Tst/service_test.py
+++ Tst/service_test.py
@@ -1,71 +1,258 @@
-from Src.Logics.storage_prototype import storage_prototype
+from Src.Logics.storage_service import storage_service
 from Src.Logics.start_factory import start_factory
-import unittest
 from Src.settings_manager import settings_manager
 from Src.Storage.storage import storage
+from Src.exceptions import operation_exception
+from Src.Models.unit_model import unit_model
 from datetime import datetime
-from Src.Logics.storage_service import storage_service
+import unittest
 
 class service_test(unittest.TestCase):
-
-    def test_storage_service(self):
+    
+    #
+    # Проверить работу метода create_turns
+    #
+    def test_check_create_turns(self):
         # Подготовка
         manager = settings_manager()
         start = start_factory(manager.settings)
         start.create()
         key = storage.storage_transaction_key()
         data = start.storage.data[ key ]
-
+        service = storage_service(data)
         start_date = datetime.strptime("2024-01-01", "%Y-%m-%d")
         stop_date = datetime.strptime("2024-01-10", "%Y-%m-%d")
-        service = storage_service(data)
-
+        
         # Действие
-        filtered = service.create_turns(start_date, stop_date)
-
-        # Проверка
-        print(filtered)
-
-        assert filtered is not None
-        assert len(filtered) > 0
-        assert isinstance(filtered, list)
-
-
-    def test_service_filter(self):
+        result = service.create_turns(start_date, stop_date)
+        
+        # Проверки
+        assert len(result) > 0
+        
+    #
+    # Проверить метод     create_turns_by_nomenclature
+    #
+    def test_check_create_turns_by_nomenclature(self):
         # Подготовка
         manager = settings_manager()
         start = start_factory(manager.settings)
         start.create()
         key = storage.storage_transaction_key()
         data = start.storage.data[ key ]
+        service = storage_service(data)
+        start_date = datetime.strptime("2022-01-01", "%Y-%m-%d")
+        stop_date = datetime.strptime("2029-12-30", "%Y-%m-%d")
         
-        start_date = datetime.strptime("2024-01-01", "%Y-%m-%d")
-        stop_date = datetime.strptime("2024-01-30", "%Y-%m-%d")
-        nomen_id = data[0].nomenclature.id
+        if len(data) == 0:
+            raise operation_exception("Набор данных пуст!")
+        
+        nomenclature = data[0].nomenclature 
+        
+        # Действие
+        result = service.create_turns_by_nomenclature(start_date, stop_date, nomenclature )
+        
+        # Проверки
+        assert len(result) == 1
+            
+    #
+    # Проверить метод  turns_only_nomenclature
+    #    
+    def test_check_create_turns_only_nomenclature(self):
+        # Подготовка
+        manager = settings_manager()
+        start = start_factory(manager.settings)
+        start.create()
+        key = storage.storage_transaction_key()
+        data = start.storage.data[ key ]
         service = storage_service(data)
         
+        if len(data) == 0:
+            raise operation_exception("Набор данных пуст!")
         
-        # Дейтсвие
-        result = service.create_turns_by_nomen(start_date, stop_date, nomen_id) 
+        nomenclature = data[0].nomenclature 
         
-        # Проверка
-        print(result)
+        # Действие
+        result = service.create_turns_only_nomenclature( nomenclature )
+        
+        # Проверки
+        assert len(result) > 0
+        
+    #
+    # Проверить работу метода    create_turns_by_receipt
+    # 
+    def test_check_create_turns_by_receipt(self):
+        # Подготовка
+        manager = settings_manager()
+        start = start_factory(manager.settings)
+        start.create()
+        key = storage.storage_transaction_key()
+        transactions_data = start.storage.data[ key ]
+        service = storage_service(transactions_data)
+        
+        if len(transactions_data) == 0:
+            raise operation_exception("Набор данных пуст!")
+        
+        key = storage.receipt_key()
+        receipts_data = start.storage.data[ key ]
+        
+        if len(receipts_data) == 0:
+            raise operation_exception("Набор данных пуст!")
+        
+        receipt = receipts_data[0]
+        
+        # Действие
+        result = service.create_turns_by_receipt(receipt)
+        
+        # Проверки
         assert len(result) > 0
 
-    def test_service_transactions(self):
+        
+    #
+    # Проверить метод  build_debits_by_receipt. Ошибочный сценарий.
+    #   
+    def test_check_build_debits_by_receipt_fail(self):
+        # Подготовка
+        manager = settings_manager()
+        start = start_factory(manager.settings)
+        start.create()
+        key = storage.storage_transaction_key()
+        transactions_data = start.storage.data[ key ]
+        service = storage_service(transactions_data)
+        
+        if len(transactions_data) == 0:
+            raise operation_exception("Набор данных пуст!")
+        
+        key = storage.receipt_key()
+        receipts_data = start.storage.data[ key ]
+        
+        if len(receipts_data) == 0:
+            raise operation_exception("Набор данных пуст!")
+        
+        # -> Цезарь с курицей
+        receipt = receipts_data[1]
+        
+        # Действие и проверка
+        with self.assertRaises(operation_exception):
+            service.build_debits_by_receipt( receipt )
+            
+            
+    #
+    # Проверить метод  build_debits_by_receipt. Корректный сценарий
+    #   
+    def test_check_build_debits_by_receipt_pass(self):
+        # Подготовка
+        manager = settings_manager()
+        start = start_factory(manager.settings)
+        start.create()
+        key = storage.storage_transaction_key()
+        transactions_data = start.storage.data[ key ]
+        start_len_transaction = len(transactions_data)
+        service = storage_service(transactions_data)
+        
+        if len(transactions_data) == 0:
+            raise operation_exception("Набор данных пуст!")
+        
+        key = storage.receipt_key()
+        receipts_data = start.storage.data[ key ]
+        
+        if len(receipts_data) == 0:
+            raise operation_exception("Набор данных пуст!")
+        
+        # -> Вафли хрустящие в вафильнице
+        receipt = receipts_data[0]
+        
+        # Действие и проверка
+        service.build_debits_by_receipt( receipt ) 
+        stop_len_transaction = len(start.storage.data[  storage.storage_transaction_key() ])
+          
+        # Проверка (транзакций должно быть больше)   
+        assert start_len_transaction < stop_len_transaction   
+        
+    def test_check_create_turns_by_nomenclature(self):
         # Подготовка
         manager = settings_manager()
         start = start_factory(manager.settings)
         start.create()
         key = storage.storage_transaction_key()
         data = start.storage.data[ key ]
-        receipt = start.storage.data[storage.receipt_key()][0]
         service = storage_service(data)
+        start_date = datetime.strptime("2022-01-01", "%Y-%m-%d")
+        stop_date = datetime.strptime("2029-12-30", "%Y-%m-%d")
         storage_ = data[0].storage
         
-        # Дейтсвие
-        result = service.create_transactions(receipt, storage_)
+        if len(data) == 0:
+            raise operation_exception("Набор данных пуст!")
         
-        # Проверка
-        print(result)
-        assert len(result) > 0
+        nomenclature = data[0].nomenclature 
+        
+        # Действие
+        result = service.create_turns_by_nomenclature_and_storage(start_date, stop_date, nomenclature, storage_ )
+        
+        # Проверки
+        assert len(result) == 1
+            
+            
+        
+    #
+    # Проверить метод  build_debits_by_receipt. Ошибочный сценарий. (обьем превышает оборот)
+    #   
+    def test_check_build_debits_fail(self):
+        # Подготовка
+        manager = settings_manager()
+        start = start_factory(manager.settings)
+        start.create()
+        key = storage.storage_transaction_key()
+
+        transactions_data = start.storage.data[ key ]
+        transactions_data[0].unit = unit_model.create_gram()
+        
+        service = storage_service(transactions_data)
+        
+        if len(transactions_data) == 0:
+            raise operation_exception("Набор данных пуст!")
+        
+        key = storage.receipt_key()
+        receipts_data = start.storage.data[ key ]
+        
+        if len(receipts_data) == 0:
+            raise operation_exception("Набор данных пуст!")
+        
+        receipt = receipts_data[0]
+        
+        # Действие и проверка
+        with self.assertRaises(operation_exception):
+            service.build_debits_by_receipt( receipt )
+            
+            
+    #
+    # Проверить метод  build_debits_by_receipt. Корректный сценарий. Обьем не превышает оборот
+    #   
+    def test_check_build_debits_pass(self):
+        # Подготовка
+        manager = settings_manager()
+        start = start_factory(manager.settings)
+        start.create()
+        key = storage.storage_transaction_key()
+        transactions_data = start.storage.data[ key ]
+        start_len_transaction = len(transactions_data)
+        service = storage_service(transactions_data)
+        
+        if len(transactions_data) == 0:
+            raise operation_exception("Набор данных пуст!")
+        
+        key = storage.receipt_key()
+        receipts_data = start.storage.data[ key ]
+        
+        if len(receipts_data) == 0:
+            raise operation_exception("Набор данных пуст!")
+        
+        # -> Вафли хрустящие в вафильнице
+        receipt = receipts_data[0]
+        
+        # Действие и проверка
+        service.build_debits_by_receipt( receipt ) 
+        stop_len_transaction = len(start.storage.data[  storage.storage_transaction_key() ])
+          
+        # Проверка (транзакций должно быть больше)   
+        assert start_len_transaction < stop_len_transaction   
+        
